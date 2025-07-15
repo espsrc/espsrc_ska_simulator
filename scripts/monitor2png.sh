701ccaee36c89018ef2f2f444918f20a5315b81e
Dani Díaz <ddiaz@iaa.es>
2025-07-15 10:12:16 +0000

    Dani

---
+++ scripts/monitor2png.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo $1
+python monitor2png.py $1 --folder monitor_png
---
+++ scripts/sorted.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Sorting files..."
+sleep 3
+if [[ -n "$1" ]]; then
+    BASE_DIR="$1"
+else
+    BASE_DIR="."
+fi
+cd "$BASE_DIR" || exit 1
+for file in *; do
+    if [[ -f "$file" && "$file" =~ ^([0-9]{8}_[0-9]{4}) ]]; then
+        folder="${BASH_REMATCH[1]}"
+        mkdir -p "$folder"
+        rm -rf *.tmp
+        mv "$file" "$folder/"
+        echo "Movido: $file → $folder/"
+    fi
+done
+
+
